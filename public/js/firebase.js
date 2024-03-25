23566d8a36b3e9d2e6800a3b7eed69f0e325c6f8
honeyy02 <honeysharma0224@gmail.com>
2024-03-25 16:45:08 +0530

    intial commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBCJT6inKEZCWviiHNHxsntBaioWstfL44",
+  authDomain: "blog-fc4de.firebaseapp.com",
+  projectId: "blog-fc4de",
+  storageBucket: "blog-fc4de.appspot.com",
+  messagingSenderId: "86403944736",
+  appId: "1:86403944736:web:b9be6b6c5ece630bdf8745"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
